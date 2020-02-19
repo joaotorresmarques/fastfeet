20238a8756c057e602be4709ba957001deca8261
SkyHeads <skyheads@live.com>
2020-02-19 17:08:00 -0300

    Create Model Recipient and Controller + Route

---
+++ backend/src/app/controllers/RecipientController.js
@@ -0,0 +1,53 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const { name } = req.body;
+
+    const recipient = await Recipient.findOne({
+      where: { name },
+    });
+
+    if (recipient) {
+      return res.status(400).json({ error: 'Recipient name already exists' });
+    }
+
+    const {
+      id,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
+  }
+}
+
+export default new RecipientController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -16,5 +17,7 @@ routes.use(authMiddleware);
 routes.get('/users', UserController.index);
 routes.put('/users', UserController.update);
 routes.delete('/users/:userId', UserController.delete);
+
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
