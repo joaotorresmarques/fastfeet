c7e07862bf78e5be9bd9ea14939bc365995add40
SkyHeads <skyheads@live.com>
2020-02-20 01:09:34 -0300

    Add multer and multerConfig

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -7,6 +9,7 @@ import RecipientController from './app/controllers/RecipientController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 
@@ -21,4 +24,8 @@ routes.delete('/users/:userId', UserController.delete);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:recipientId', RecipientController.update);
 
+routes.post('/files', upload.single('file'), (req, res) => {
+  console.log(req.file);
+  return res.json({ ok: true });
+});
 export default routes;
