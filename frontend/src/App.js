deaff3885b084849a5cc4c1bbcb5ac686af1a124
SkyHeads <skyheads@live.com>
2020-04-10 18:17:32 -0300

    GoBarber Web First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <>
+    <h1>Hello World FastFeet</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+   </>
+  );
+}
+
+export default App;
