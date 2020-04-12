47231160e1e2ed0853d6d854cafdf843011ccbfa
SkyHeads <skyheads@live.com>
2020-04-12 17:15:23 -0300

    SignIn Page and Header/Input Component

--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  width: 100%;
+  height: 100%;
   background: #7159c1;
   display: flex;
   align-items: center;
@@ -10,6 +10,48 @@ export const Wrapper = styled.div`
 
 export const Content = styled.div`
   width: 100%;
-  max-width: 315px;
+  max-width: 350px;
   text-align: center;
+  border-radius: 4px;
+  text-align: center;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+  align-items: center;
+  padding: 45px 20px;
+
+  img {
+    width: 100%;
+    margin-top: 10px;
+    max-width: 230.85px;
+    height: 40px;
+    margin-bottom: 10px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 25px;
+    padding: 0 8px;
+
+    label + label {
+      margin-top: 10px;
+    }
+
+    button {
+      height: 44px;
+      background: #7159c1;
+      border: 0;
+      border-radius: 4px;
+      font-weight: normal;
+      color: #ffffff;
+      margin: -1px 0 10px;
+      font-size: 15px;
+      transition: background 0.2s;
+      margin-top: 10px;
+
+      &:hover {
+        background: #7d40e7;
+      }
+    }
+  }
 `;
