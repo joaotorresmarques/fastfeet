b3d6b2279f06d4dd84f530c4fd8119a976b418bc
Bruno Fernandes <skyheads@live.com>
2020-05-10 01:53:05 -0300

    simpleinput component

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { Form } from '@unform/web';
+
 import logo from '../../assets/logo.svg';
 
 export default function SignIn() {
   return (
     <> 
       <img src={logo} alt="FastFeet"/>
+
+      <Form onSubmit={() => {}}>
+
+      </Form>
     </>
   );
 }
