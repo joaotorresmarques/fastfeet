158d6404420e819ac52dfa6fd7c39dbfea3e57f9
Bruno Fernandes <skyheads@live.com>
2020-05-10 02:06:13 -0300

    simpleinput component and add styles/colors

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -4,13 +4,27 @@ import { Form } from '@unform/web';
 
 import logo from '../../assets/logo.svg';
 
+import SimpleInput  from '../../components/Form/SimpleInput';
+
 export default function SignIn() {
   return (
     <> 
       <img src={logo} alt="FastFeet"/>
 
       <Form onSubmit={() => {}}>
+        <SimpleInput
+          name="email"
+          label="SEU E-MAIL"
+          type="email"
+          placeholder="exemplo@email.com"
+        />
 
+        <SimpleInput
+          name="password"
+          label="SUA SENHA"
+          type="password"
+          placeholder="*************"
+        />
       </Form>
     </>
   );
