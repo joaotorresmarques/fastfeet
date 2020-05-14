a6c4554f9d68109d96a329f94ee3db098161db7f
Bruno Fernandes <skyheads@live.com>
2020-05-14 20:52:37 -0300

    morepopup component

--- frontend/src/pages/Delivery/DeliveryItem/index.js
+++ frontend/src/pages/Delivery/DeliveryItem/index.js
@@ -1,7 +1,58 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import PropTypes from 'prop-types';
 
-export default function DeliveryItem() {
-  return <div />;
+import { Container, MoreConainer } from './styles';
+
+export default function DeliveryItem({ updateDeliveries, data }) {
+  return (
+    <Container>
+			<small>#{data.id}</small>
+			<small>{data.recipient.name}</small>
+			<small>{data.product}</small>
+			<small>{data.recipient.city}</small>
+			<small>{data.recipient.state}</small>
+			<Status
+				text={data.status}
+				color={statusColors[data.status].color}
+				background={statusColors[data.status].background}
+			/>
+			<More>
+				<MoreConainer>
+					<div>
+						<DeliveryModal data={data} />
+					</div>
+					<div>
+						<button
+							onClick={() => history.push(`/deliveries/form/${data.id}`)}
+							type="button"
+						>
+							<MdEdit color={colors.info} size={15} />
+							<span>Editar</span>
+						</button>
+					</div>
+					<div>
+						<button onClick={handleDelete} type="button">
+							<MdDeleteForever color={colors.danger} size={15} />
+							<span>Excluir</span>
+						</button>
+					</div>
+				</MoreConainer>
+			</More>
+		</Container>
+  );
 }
+
+DeliveryItem.propTypes = {
+	updateDeliveries: PropTypes.func.isRequired,
+	data: PropTypes.shape({
+		id: PropTypes.number,
+		product: PropTypes.string,
+		recipient: PropTypes.shape({
+			name: PropTypes.string,
+			city: PropTypes.string,
+			state: PropTypes.string,
+		}),
+		status: PropTypes.string,
+	}).isRequired,
+};
