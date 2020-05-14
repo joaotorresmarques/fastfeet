ad0ec89a864d7a78c3e0c0096017316789223723
Bruno Fernandes <skyheads@live.com>
2020-05-14 00:15:47 -0300

    searchinput component

--- frontend/src/components/Form/SearchList/index.js
+++ frontend/src/components/Form/SearchList/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
 import { Container } from './styles';
 
-export default function SearchList() {
+export default function SearchList({ ...rest }) {
   return (
     <Container>
-      
+      <MdSearch color="#999" size={16} />
+      <input {...rest} />
     </Container>
   );
 }
