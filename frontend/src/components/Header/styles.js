c6341290eaa11ab0ee4ccdc185601648e57c2271
Bruno Fernandes <skyheads@live.com>
2020-05-13 21:32:11 -0300

    container in styles

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
   background: #f0f0f0;
+  padding: 0 30px;
+
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 35px;
 `;
