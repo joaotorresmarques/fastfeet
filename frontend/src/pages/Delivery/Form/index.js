7cdafab49cec19a3830c291c18f43f10df8f559c
Bruno Fernandes <skyheads@live.com>
2020-05-15 23:38:25 -0300

    header form component

--- frontend/src/pages/Delivery/Form/index.js
+++ frontend/src/pages/Delivery/Form/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Container, Content } from './styles';
 
+import HeaderForm from '../../../components/HeaderForm';
+
 export default function DeliveryForm() {
   return (
     <Container>
       <Content>
-        DeliveryForm
+        <HeaderForm title="Cadastro de encomendas"/>
       </Content>
     </Container>
   );
