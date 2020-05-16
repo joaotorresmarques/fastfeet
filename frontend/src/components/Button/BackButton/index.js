3955ca679b17450aa5df83804141c494987f802e
Bruno Fernandes <skyheads@live.com>
2020-05-15 23:44:09 -0300

    backbutton component

---
+++ frontend/src/components/Button/BackButton/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+
+import history from '../../../services/history';
+
+// import { Container } from './styles';
+
+export default function BackButton() {
+  return (
+    <IconButton
+
+    />
+  );
+}
