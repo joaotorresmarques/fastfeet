dc80535e86c33d340a1b9096fd3433c523a747ac
Bruno Fernandes <skyheads@live.com>
2020-05-16 00:19:45 -0300

    import backbutton

--- frontend/src/components/Button/BackButton/index.js
+++ frontend/src/components/Button/BackButton/index.js
@@ -3,12 +3,17 @@ import { MdKeyboardArrowLeft } from 'react-icons/md';
 
 import history from '../../../services/history';
 
+import IconButton from '../IconButton';
+
 // import { Container } from './styles';
 
 export default function BackButton() {
   return (
     <IconButton
-
+      title="VOLTAR"
+      Icon={MdKeyboardArrowLeft}
+      action={history.goBack}
+      background="#CCC"
     />
   );
 }
--- frontend/src/pages/Delivery/Form/index.js
+++ frontend/src/pages/Delivery/Form/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Container, Content } from './styles';
 
 import HeaderForm from '../../../components/HeaderForm';
+import BackButton from '../../../components/Button/BackButton';
 
 export default function DeliveryForm() {
   return (
     <Container>
       <Content>
         <HeaderForm title="Cadastro de encomendas"/>
+        <BackButton />
       </Content>
     </Container>
   );
