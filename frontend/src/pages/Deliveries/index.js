7d6d6698f1fb53516efeea0c24077efc87adbfe6
Bruno Fernandes <skyheads@live.com>
2020-05-21 23:43:43 -0300

    initial search input/ html for header deliveries

--- frontend/src/pages/Deliveries/index.js
+++ frontend/src/pages/Deliveries/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import SearchInput from '../../components/SearchInput';
+
+import { Container, PageTitle } from './styles';
 
 export default function Deliveries() {
   return (
-    <h1>Deliveries</h1>
+    <Container>
+      <header>
+        <PageTitle>Gerenciando encomendas</PageTitle>
+        <div>
+          <SearchInput />
+          <Link to="/deliveries">
+            Cadastrar
+          </Link>
+        </div>
+      </header>
+    </Container>
   );
 }
--- frontend/src/pages/Deliveries/styles.js
+++ frontend/src/pages/Deliveries/styles.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  header {
+    margin-bottom: 34px;
+  }
+`;
+
+export const PageTitle = styled.h1`
+  color: #444444;
+  font-size: 24px;
+  font-weight: bold;
 `;
